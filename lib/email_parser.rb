2a96002ca11f1e0fbc6bd2937bac1d00b300eadc
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-29 03:47:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
   def parse
     email_array = @all_emails.split(/,|\s|[, ]/)
     email_array.select do |email|
-      email.delete if email==""
+      email.delete_if("")
     end
     email_array.uniq
   end
